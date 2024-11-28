37a46a0ac5cef534cd94633af07088790ca87fe3
Aamir <2270587@cognizant.com>
2024-11-29 05:13:47 +0530

    made test changes

--- test.js
+++ test.js
@@ -6,3 +6,11 @@
  console.log(output, typeof output);
 
  console.log("harry");
+ console.dir(document);
+ console.log(document.links[0].innerHTML);
+//  document.links[0].innerHTML="<h1>iop</h1>";
+//  document.write("hello wij")
+const linkURL = document.getElementById("mad");
+// linkURL.innerText = "kl";
+document.querySelector("#mad a").innerHTML = "jk"
+console.log(linkURL)
